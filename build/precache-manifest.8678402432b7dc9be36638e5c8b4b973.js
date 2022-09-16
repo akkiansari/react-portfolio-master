e3a92ba6c32da06be10ba5bc8d80fa60ddb4c203
Aqueel-ansari <aqueela73@gmail.com>
2022-09-16 13:53:40 +0530

    build

---
+++ build/precache-manifest.8678402432b7dc9be36638e5c8b4b973.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fedcecb64f63cf8bddfcf951d66563",
+    "url": "/akkiansari/myPort/index.html"
+  },
+  {
+    "revision": "1960a2817c192b711c34",
+    "url": "/akkiansari/myPort/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "f5df5dc12a9a23c3123b",
+    "url": "/akkiansari/myPort/static/css/main.82735d22.chunk.css"
+  },
+  {
+    "revision": "1960a2817c192b711c34",
+    "url": "/akkiansari/myPort/static/js/2.7f7e6978.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/akkiansari/myPort/static/js/2.7f7e6978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5df5dc12a9a23c3123b",
+    "url": "/akkiansari/myPort/static/js/main.7943c8cc.chunk.js"
+  },
+  {
+    "revision": "522b11a85ca8b9b2f782",
+    "url": "/akkiansari/myPort/static/js/runtime-main.d5ca0f56.js"
+  },
+  {
+    "revision": "fa226a15b526036550a806017109d74f",
+    "url": "/akkiansari/myPort/static/media/Medical.fa226a15.jpg"
+  },
+  {
+    "revision": "8b8eac82fb5d5b45621e03d03ac3c873",
+    "url": "/akkiansari/myPort/static/media/Port1.8b8eac82.jpg"
+  },
+  {
+    "revision": "8a41caa1d29726554642b4c6a753353f",
+    "url": "/akkiansari/myPort/static/media/bmi.8a41caa1.jpg"
+  },
+  {
+    "revision": "93782f6947bb5ea703f7d7e108aa4230",
+    "url": "/akkiansari/myPort/static/media/curd.93782f69.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/akkiansari/myPort/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/akkiansari/myPort/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/akkiansari/myPort/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/akkiansari/myPort/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/akkiansari/myPort/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/akkiansari/myPort/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/akkiansari/myPort/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/akkiansari/myPort/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/akkiansari/myPort/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/akkiansari/myPort/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/akkiansari/myPort/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/akkiansari/myPort/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/akkiansari/myPort/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "ccf25430b8055f78bf2c062862b56c6d",
+    "url": "/akkiansari/myPort/static/media/market.ccf25430.jpg"
+  }
+]);
