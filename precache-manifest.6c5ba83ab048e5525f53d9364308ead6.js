b64aadd01c08bfb5b06ed344e18744817fe0c1bb
Aqueel-ansari <aqueela73@gmail.com>
2022-09-16 14:09:09 +0530

    Updates

---
+++ precache-manifest.6c5ba83ab048e5525f53d9364308ead6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e1bed42af656df2159695529ccab0d",
+    "url": "/myPort/index.html"
+  },
+  {
+    "revision": "269e8f0593c08041c9dc",
+    "url": "/myPort/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "5eceaa663771720cd05d",
+    "url": "/myPort/static/css/main.fe3b7bf3.chunk.css"
+  },
+  {
+    "revision": "269e8f0593c08041c9dc",
+    "url": "/myPort/static/js/2.f583ff8c.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/myPort/static/js/2.f583ff8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eceaa663771720cd05d",
+    "url": "/myPort/static/js/main.6c5b1e7c.chunk.js"
+  },
+  {
+    "revision": "b026856fddc0877ad522",
+    "url": "/myPort/static/js/runtime-main.4bc190d2.js"
+  },
+  {
+    "revision": "fa226a15b526036550a806017109d74f",
+    "url": "/myPort/static/media/Medical.fa226a15.jpg"
+  },
+  {
+    "revision": "8b8eac82fb5d5b45621e03d03ac3c873",
+    "url": "/myPort/static/media/Port1.8b8eac82.jpg"
+  },
+  {
+    "revision": "8a41caa1d29726554642b4c6a753353f",
+    "url": "/myPort/static/media/bmi.8a41caa1.jpg"
+  },
+  {
+    "revision": "93782f6947bb5ea703f7d7e108aa4230",
+    "url": "/myPort/static/media/curd.93782f69.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/myPort/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myPort/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myPort/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myPort/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myPort/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myPort/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/myPort/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/myPort/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/myPort/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/myPort/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/myPort/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/myPort/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/myPort/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "ccf25430b8055f78bf2c062862b56c6d",
+    "url": "/myPort/static/media/market.ccf25430.jpg"
+  }
+]);
